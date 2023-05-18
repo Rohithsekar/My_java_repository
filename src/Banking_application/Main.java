3195caabcaaff1fcef504effd59133fa0531c4d4
rohith <rohithsekar@gmail.com>
2023-05-18 17:56:56 +0530

    Deleted Admin and Main_menu classes. Changed the ArrayLists to static arrays to store user objects and account numbers and linked list to a String array to store log messages.

--- src/Banking_application/Main.java
+++ src/Banking_application/Main.java
@@ -1,17 +1,16 @@
 package Banking_application;
 
-import java.util.ArrayList;
+
 
 public class Main
 {
-    public static  ArrayList<Admin> admins = new ArrayList<>();
-    public static ArrayList<User> users = new ArrayList<>();
+    public static int user_count =0;
+    public static User[] users = new User[10];
     public static void main(String[] args)
     {
         System.out.println("Welcome to xyz bank");
-        Admin admin1 = new Admin("a","a"); //creating an admin with username and password
-        Admin.add_admin(admin1);
-        Main_menu.main_menu(); //calling the main_menu method in Main_menu class
+        //Main_menu.main_menu(); //calling the main_menu method in Main_menu class
+        User.user_page();
     }
 
 }
